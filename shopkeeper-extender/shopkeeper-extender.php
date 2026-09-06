7dd0a559baef9ff57d2c5a281ba55192e8d763fa
Sean Murphy <sean@iamseanmurphy.com>
2026-09-06 19:07:20 +0000

    10.0.6

--- shopkeeper-extender/shopkeeper-extender.php
+++ shopkeeper-extender/shopkeeper-extender.php
@@ -4,7 +4,7 @@
  * Plugin Name:       		Shopkeeper Extender
  * Plugin URI:        		https://shopkeeper.getbowtied.com
  * Description:       		Extends the functionality of Shopkeeper with theme specific features.
- * Version:           		10.0.1
+ * Version:           		10.0.6
  * Author:            		Get Bowtied
  * Author URI:				https://getbowtied.com
  * License:					GPL v2 or later
@@ -37,8 +37,6 @@ if ( ! class_exists( 'ShopkeeperExtender' ) ) :
 			if (self::$initialized) {
 				return;
 			}
-
-			require_once dirname( __FILE__ ) . '/core/theme-updater/class-gbt-extender-theme-updater.php';
 
 			if ( ! function_exists( 'is_plugin_active' ) ) {
 				require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
@@ -107,6 +105,9 @@ if ( ! class_exists( 'ShopkeeperExtender' ) ) :
 	}
 
 endif;
+
+// Theme updater: newest library version among active companions wins on plugins_loaded.
+require_once dirname( __FILE__ ) . '/core/theme-updater/loader.php';
 
 add_action( 'after_setup_theme', function() {
     ShopkeeperExtender::init();
